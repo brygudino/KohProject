d5b3b02da2d4ba05a713a16d152678ab819c63b3
brygudino <42814759+brygudino@users.noreply.github.com>
2019-12-03 18:00:59 -0800

    Add files via upload

---
+++ StudentEmail.java
@@ -0,0 +1,67 @@
+public class StudentEmail
+{
+   public static final String EMAIL_STUB = "@my.email";
+
+   private String name;
+   private String id;
+
+   public StudentEmail ()
+   {
+      name = "";
+      id   = "";
+   }
+
+   public StudentEmail (String name, String id)
+                        throws StudentEmailException
+   {
+      // Remove elading and trailing spaces, tabs.
+      name = name.trim ();
+      id   = id.trim ();
+
+      if (name.length () == 0)
+      {
+         //JOptionPane.showMessageDialog (null, "Error: name cannot be blank.");
+         throw new StudentEmailException ("Error: name cannot be blank.");
+      }
+
+      else if (id.length () == 0)
+      {
+         //JOptionPane.showMessageDialog (null, "Error: Id cannot be blank.");
+         throw new StudentEmailException ("Error: Id cannot be blank.");
+      }
+
+      else
+      {
+         // All is OK, set class data to the values passed in.
+         this.name = name;
+         this.id   = id;
+      }
+   }
+
+   public String getName ()
+   {
+      return name;
+   }
+
+   public String getId ()
+   {
+      return id;
+   }
+
+   public void setName (String name)
+   {
+      this.name = name;
+   }
+
+   public void setId (String id)
+   {
+      this.id = id;
+   }
+
+   @Override
+   public String toString ()
+   {
+      return id + "\t" + name + "\t" + id + EMAIL_STUB;
+   }
+
+} // public class StudentEmail
---
+++ StudentEmailException.java
@@ -0,0 +1,23 @@
+public class StudentEmailException extends Exception
+{
+
+   public StudentEmailException ()
+   {
+      super ();
+   }
+
+   public StudentEmailException (String message)
+   {
+      super (message);
+   }
+
+   public StudentEmailException (Throwable cause)
+   {
+      super (cause);
+   }
+
+   public StudentEmailException (String message, Throwable cause)
+   {
+      super (message, cause);
+   }
+}
